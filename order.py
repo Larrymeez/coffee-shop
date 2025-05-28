c5b320d8a966416b1e0b48324492052930dfc963
Larrymeez <meezlarry@gmail.com>
2025-05-28 19:28:30 +0300

    final commit

--- order.py
+++ order.py
@@ -1,18 +1,21 @@
 class Order:
-    all = []
+    all_orders = []
 
     def __init__(self, customer, coffee, price):
-        if not isinstance(customer, object) or not hasattr(customer, "name"):
-            raise TypeError("Invalid Customer instance.")
-        if not isinstance(coffee, object) or not hasattr(coffee, "name"):
-            raise TypeError("Invalid Coffee instance.")
+        from customer import Customer
+        from coffee import Coffee
+
+        if not isinstance(customer, Customer):
+            raise TypeError("customer must be a Customer instance.")
+        if not isinstance(coffee, Coffee):
+            raise TypeError("coffee must be a Coffee instance.")
         if not isinstance(price, float) or not (1.0 <= price <= 10.0):
-            raise ValueError("Price must be a float between 1.0 and 10.0.")
+            raise ValueError("price must be a float between 1.0 and 10.0.")
 
         self._customer = customer
         self._coffee = coffee
         self._price = price
-        Order.all.append(self)
+        Order.all_orders.append(self)
 
     @property
     def customer(self):
